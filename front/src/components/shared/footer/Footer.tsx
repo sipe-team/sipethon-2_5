600182dafcf3998304594be80b52979a3dd71ae7
seokzin <seokzin95@gmail.com>
2024-05-25 01:22:45 +0900

    feat: footer active 스타일링

--- front/src/components/shared/footer/Footer.tsx
+++ front/src/components/shared/footer/Footer.tsx
@@ -1,37 +1,40 @@
 import styled from '@emotion/styled'
+import { usePathname, useRouter } from 'next/navigation'
 
 import { Icon } from '@/components'
 
 const Footer = () => {
+  const router = useRouter()
+  const pathName = usePathname()
+
+  console.log('pathName', pathName)
+  const isActivePath = (path: string) => path === pathName
+
   return (
     <Container>
       <Icon
         name="clock"
         size={24}
-        onClick={() => {
-          console.log('click')
-        }}
+        color={isActivePath('/timer') ? 'neutral900' : 'neutral400'}
+        onClick={() => router.push('/timer')}
       />
       <Icon
         name="home"
         size={24}
-        onClick={() => {
-          console.log('click')
-        }}
+        color={isActivePath('/routine') ? 'neutral900' : 'neutral400'}
+        onClick={() => router.push('/routine')}
       />
       <Icon
         name="chart"
         size={24}
-        onClick={() => {
-          console.log('click')
-        }}
+        color={isActivePath('/history') ? 'neutral900' : 'neutral400'}
+        onClick={() => router.push('/history')}
       />
       <Icon
         name="profile"
         size={24}
-        onClick={() => {
-          console.log('click')
-        }}
+        color={isActivePath('/profile') ? 'neutral900' : 'neutral400'}
+        onClick={() => router.push('/profile')}
       />
     </Container>
   )
