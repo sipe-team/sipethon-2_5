c7708be6ce9be03b57e8f9a8739fe1babc475174
sangwooyang <dndb3599@gmail.com>
2024-05-25 00:02:57 +0900

    feat(queries): fetch 설정

---
+++ front/src/queries/fetch.ts
@@ -0,0 +1,41 @@
+// queries/createFetch.ts
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+interface FetchOptions extends RequestInit {
+  token?: string
+}
+
+const defaultHeaders: HeadersInit = {
+  'Content-Type': 'application/json',
+}
+
+// 클라이언트에서만 사용되는 함수, 서버사이드에서는 따로 사용,
+const getToken = (): string | null => {
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem('token')
+  }
+  return null
+}
+
+export const _fetch = async <T>(path: string, options: FetchOptions = {}): Promise<T> => {
+  const token = options.token || getToken()
+  const headers: Headers = new Headers({
+    ...defaultHeaders,
+    ...options.headers,
+  })
+
+  if (token) {
+    headers.append('Authorization', `Bearer ${token}`)
+  }
+
+  const response = await fetch(`${API_URL}${path}`, {
+    ...options,
+    headers,
+  })
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch')
+  }
+
+  return response.json()
+}
