82f817b5b5ce7abd7435cd70377ab6aac0540a00
mike2ox <firefinger07@gmail.com>
2024-05-25 01:42:09 +0900

    feat: history api 제작

--- front/src/queries/api.ts
+++ front/src/queries/api.ts
@@ -4,10 +4,20 @@ export const fetchTodos = async () => {
   return await _fetch('/todo')
 }
 
+// Routine API
 export const getRoutines = async () => {
   return await _fetch('/routine')
 }
 
 export const getRoutine = async (id: number) => {
   return await _fetch(`/routine/${id}`)
+}
+
+// History API
+export const getHistories = async () => {
+  return await _fetch('/history')
+}
+
+export const getHistory = async (id: number) => {
+  return await _fetch(`/history/${id}`)
 }
