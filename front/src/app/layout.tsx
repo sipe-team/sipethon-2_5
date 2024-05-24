96413a6d0e9ee9fdf20d2243b043252f50331081
mike2ox <firefinger07@gmail.com>
2024-05-24 23:17:03 +0900

    feat: 자동으로 jwt token붙여주는 queryClient 옵션 설정

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -8,9 +8,29 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Popup } from '@/components'
 import { useDarkModeStore } from '@/store'
 import { global, theme } from '@/styles'
+import { useRef } from 'react'
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient()
+  const queryClient = useRef<QueryClient>()
+  if (!queryClient.current) {
+    queryClient.current = new QueryClient({
+      defaultOptions: {
+        queries: {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          queryFn: async ({ queryKey }: { queryKey: any }) => {
+            const [url] = queryKey
+            const response = await fetch(url, {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+              },
+            })
+            return response.json()
+          },
+          retry: 1,
+        },
+      },
+    })
+  }
 
   const { isDarkMode } = useDarkModeStore()
 
@@ -22,7 +42,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <meta httpEquiv="Pragma" content="no-cache" />
       </head>
       <body>
-        <QueryClientProvider client={queryClient}>
+        <QueryClientProvider client={queryClient.current}>
           <ThemeProvider theme={theme(isDarkMode ? 'dark' : 'light')}>
             <Container>
               <Global styles={global} />
