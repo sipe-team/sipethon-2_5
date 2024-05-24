0e8a3c108312c04ff6e65bafa44d6c735de70979
sangwooyang <dndb3599@gmail.com>
2024-05-25 01:53:22 +0900

    feat(timer): timer default page 완료

--- front/src/app/timer/page.tsx
+++ front/src/app/timer/page.tsx
@@ -1,14 +1,26 @@
 'use client'
+import { useEffect, useState } from 'react'
+
 import { useTimer } from '@/hooks'
 
 const TimerPage = () => {
-  const { remainingTime, status, start, stop, reset, finish } = useTimer(3) // 60초 목표 시간
+  const { remainingTime, status, start, stop, reset, finish } = useTimer(3) // 목표 시간
+
+  const [result, setResult] = useState<'success' | 'fail' | 'pending'>('pending')
+
+  useEffect(() => {
+    if (status === 'finished') {
+      // 알고리즘에따라 성공/실패 여부 판단
+      setResult(remainingTime < 0 ? 'fail' : 'success')
+    }
+  }, [remainingTime, status])
 
   return (
     <div>
       <h1>Timer</h1>
       <p>Remaining Time: {remainingTime} seconds</p>
       <p>Status: {status}</p>
+      <p>Result: {result}</p>
       <button onClick={start} disabled={status === 'running'}>
         Start
       </button>
