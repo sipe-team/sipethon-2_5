2cc645d10107dcf84de34445b77ab29e647db2c5
sangwooyang <dndb3599@gmail.com>
2024-05-25 03:34:10 +0900

    feat(front): time score 완료

--- front/src/app/timer/page.tsx
+++ front/src/app/timer/page.tsx
@@ -5,21 +5,48 @@ import ProgressCircle from '@/components/progress-circle/ProgressCircle'
 import { useTimer } from '@/hooks'
 
 const TimerPage = () => {
-  const GOALTIME = 3
-  const { remainingTime, status, start, stop, reset, finish } = useTimer(GOALTIME) // 목표 시간 GOALTIME초
+  const GOALTIME = 10 // 4분
+  const { remainingTime, time, status, start, stop, reset, finish } = useTimer(GOALTIME) // 목표 시간 GOALTIME초
   const [result, setResult] = useState<'success' | 'fail' | 'pending'>('pending')
+
+  const calculateScore = ({ remainingTime, goalTime }: { remainingTime: number; goalTime: number }): number => {
+    const absoluteDifference = Math.abs(remainingTime)
+    const percentageDifference = (absoluteDifference / goalTime) * 100
+
+    if (percentageDifference === 0) {
+      return 100
+    } else if (percentageDifference <= 10) {
+      return 100 - percentageDifference * 5 // 100에서 50까지 선형적으로 감소
+    } else {
+      return -((percentageDifference - 20) * 5) // 0에서 음수로 감소
+    }
+  }
+
+  const timeToMinutes = (time: number): string => {
+    return Math.ceil(time / 60)
+      .toFixed(0)
+      .padStart(2, '0')
+  }
+
+  const timeToMinSec = (time: number): string => {
+    const minutes = Math.floor(time / 60)
+    const seconds = (time % 60).toFixed(0).padStart(2, '0')
+    return `${minutes}:${seconds}`
+  }
 
   useEffect(() => {
     if (status === 'finished') {
       // 알고리즘에 따라 성공/실패 여부 판단
-      setResult(remainingTime < 0 ? 'fail' : 'success')
+      setResult(calculateScore({ remainingTime, goalTime: GOALTIME }) > 0 ? 'success' : 'fail')
     }
   }, [remainingTime, status])
 
   return (
     <div>
       <h1>Timer</h1>
-      <p>RemainingTime Time: {remainingTime} seconds</p>
+      <p>GoalTime Time: {timeToMinutes(GOALTIME)} min</p>
+      <p>RemainingTime Time: {timeToMinSec(remainingTime)} seconds</p>
+      <p>Time: {timeToMinSec(time)} seconds</p>
       <p>Status: {status}</p>
       <p>Result: {result}</p>
       <button onClick={start} disabled={status === 'running'}>
