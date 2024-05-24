2c71b83cd35900669d0606d88a21420b2ca30cf3
sangwooyang <dndb3599@gmail.com>
2024-05-25 01:31:06 +0900

    feat(timer): timer ploc 완료

--- front/src/app/timer/page.tsx
+++ front/src/app/timer/page.tsx
@@ -1,7 +1,26 @@
+'use client'
+import { useTimer } from '@/hooks'
+
 const TimerPage = () => {
+  const { remainingTime, status, start, stop, reset, finish } = useTimer(3) // 60초 목표 시간
+
   return (
     <div>
-      <h1>Timer page</h1>
+      <h1>Timer</h1>
+      <p>Remaining Time: {remainingTime} seconds</p>
+      <p>Status: {status}</p>
+      <button onClick={start} disabled={status === 'running'}>
+        Start
+      </button>
+      <button onClick={stop} disabled={status !== 'running'}>
+        Stop
+      </button>
+      <button onClick={finish} disabled={status !== 'running'}>
+        Finish
+      </button>
+      <button onClick={reset} disabled={status === 'running'}>
+        Reset
+      </button>
     </div>
   )
 }
