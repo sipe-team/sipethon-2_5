b53765d80dc3222818d04ecaf6e158f96ea3c005
mike2ox <firefinger07@gmail.com>
2024-05-25 01:42:09 +0900

    feat: history api 제작

---
+++ front/src/queries/history/type.ts
@@ -0,0 +1,31 @@
+/**
+ * History interface
+ *
+ * @interface History
+ * @property {number} id - 히스토리 id
+ * @property {string} title - 히스토리 제목
+ * @property {number} count - 히스토리 완료 카운트
+ *
+ */
+
+export interface History {
+  id: number
+  title: string
+  count: number
+}
+
+export interface HistoryDetail {
+  id: number
+  title: string
+  count: number
+  routine: {
+    id: number
+    name: string
+    description: string
+  }
+  historyLogs: {
+    id: number
+    count: number
+    createdAt: string
+  }[]
+}
